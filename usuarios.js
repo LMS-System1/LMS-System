b40a47094c73c235b031322685886042646950cb
LMS-System1 <ferrarilucas.lds@gmail.com>
2026-02-26 18:42:49 -0300

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -1,5 +1,6 @@
 // usuarios.js
 const DB_USUARIOS = {
     "Lucas": "3d14c2d4e4ced81e459e4ace7c01466a700000fb94a3bbe944a55fb92693e879",
-    "Agustin": "123123"
+    "Agustin": "96cae35ce8a9b0244178bf28e4966c2ce1b8385723a96a6b838858cdd6ca0a1e"
 };
+
